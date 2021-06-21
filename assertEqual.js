eb809e99cbb53a97df415239bfdd8b315d8db768
cat <catherinehu.9898@gmail.com>
2021-06-21 21:02:09 +0000

    AssertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👌👌Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
