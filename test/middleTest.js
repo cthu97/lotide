20951090209ce64ed4392f7c609c4583b614246f
Catherine Hu <catherinehu_9898@hotmail.com>
2021-06-28 21:09:42 +0000

    refactor with m&c

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,13 @@
-const middle = require('../middle')
+const middle = require('../middle');
+const assert = require('chai').assert;
 
 
-console.log(middle([1, 2, 3, 4, 5, 6]))
+describe ('#middle', () =>  {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns [3,4] for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+
+});
