8ea9efb915e4d761e2540af283fa8f1bb9a9d7e0
Catherine Hu <catherinehu_9898@hotmail.com>
2021-06-28 20:35:50 +0000

    refactor assertArraysEqual w/ test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,12 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👌👌Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌Assertion Failed: ${actual} !== ${expected}`);
-  };
-}
 
-const eqArrays = (input1, input2) => {
-  if (input1.length !== input2.length) {
-    return false;
-  }
-  for (let i = 0; i < input1.length; i++) {
-    if (input1[i] !== input2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertEqual = require('./assertEqual')
+
+const eqArrays = require('./eqArrays')
+
 
 const assertArraysEqual = (input1, input2) =>{
-  assertEqual(eqArrays(input1, input2),true);
+  assertEqual(eqArrays(input1, input2), true);
 }
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]); 
-
+module.exports = assertArraysEqual;
